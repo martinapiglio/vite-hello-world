4b42aa2012893cac52a9285a8d79cc1561c97a8e
martinapiglio <martina.piglio@gmail.com>
2023-03-23 16:16:29 +0100

    title added

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,30 @@
+<script>
+
+    export default {
+
+        data() {
+
+            return {
+
+                title: "La mia prima app con Vite!"
+
+            }
+        }
+    }
+
+</script>
+
+<template>
+
+    <h1> {{ title }} </h1>
+
+</template>
+
+<style>
+
+    h1 {
+        font-size: 3.2em;
+        line-height: 1.1;
+    }
+
+</style>
